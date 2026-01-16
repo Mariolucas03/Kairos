e0eaedfa0b61c1f213d085a28311f27b6cfac1d0
Mariolucas03 <mariolucas096@gmail.com>
2026-01-16 12:03:19 +0100

    KAIROS_BETA_01.4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        host: true,
-        port: 3000,
-    },
-    build: {
-        outDir: 'dist', // Asegura que la salida va a 'dist'
-    }
-});
+    // Esto ayuda a que Vite encuentre los archivos en Vercel
+    base: '/',
+})
